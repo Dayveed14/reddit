5cbb109fb30bdb1a4f034414c72c1ef91414ac8c
david <dedwind56@gmail.com>
2026-08-10 12:00:23 +0100

    terraform fix2

--- terraform/main.tf
+++ terraform/main.tf
@@ -102,7 +102,7 @@ data "aws_ami" "ubuntu" {
 # EC2 Instance for deploying the API
 resource "aws_instance" "api_server" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = aws_key_pair.deployer_key.key_name
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.api_sg.id]
